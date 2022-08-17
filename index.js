9b2524c9fae4b5d62973acb380de19c865b3d933
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-17 13:40:52 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(){
+     const newArray =['Antonia', 'Nuru']
+        return newArray;
+}
+function returnLastTwoDrivers(){
+    const newArray =['Amari', 'Mo']
+    return newArray;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(times){
+const fare = 2;
+return function () {
+    return Math.pow(times, fare)
+};
+}
+ function fareDoubler(fare){
+    return fare * 2;
+ }
+ function fareTripler(fare){
+    return fare * 3;
+ }
+ function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers();
+ }
